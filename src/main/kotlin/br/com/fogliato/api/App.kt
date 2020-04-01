731c2615fb65fcf6c96cdef5ac835310c3af7c1b
Fernando Fogliato <fernandofogliato@gmail.com>
2020-04-01 14:36:08 -0300

    Change database from postgres to h2

--- src/main/kotlin/br/com/fogliato/api/App.kt
+++ src/main/kotlin/br/com/fogliato/api/App.kt
@@ -1,7 +1,9 @@
 package br.com.fogliato.api
 
 import br.com.fogliato.api.config.AppConfig
+import org.h2.tools.Server
 
 fun main() {
+    Server.createWebServer().start()
     AppConfig().setup().start(7000)
 }
